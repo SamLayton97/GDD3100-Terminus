c57dbac5bddc0453720ceb9516497a5216c88700
Sam Layton <slayton@uccs.edu>
2019-09-09 15:47:50 -0600

    Added script to register fire input and activate reactive force for player

---
+++ Terminus/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Registers user's fire input, firing weapon/tool in direction
+/// of mouse and sending player in opposite direction.
+/// Note: Requires player character to have the Rigidbody2D component.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
