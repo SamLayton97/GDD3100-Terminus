67ca44f1b9b0668cb386a4cb22e98b1022530d33
Sam Layton <slayton@uccs.edu>
2019-09-09 15:17:55 -0600

    Player character rotates to face mouse position

---
+++ Terminus/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates player character to face mouse position
+/// </summary>
+public class PlayerRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // calculate angle between mouse position and player character
+        Vector2 PCToMouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)) 
+            - transform.position;
+        float angleToMouse = Mathf.Atan2(PCToMouse.y, PCToMouse.x) * Mathf.Rad2Deg;
+
+        // rotate object to face mouse position
+        transform.Rotate((Vector3.forward * angleToMouse) - transform.rotation.eulerAngles);
+
+    }
+}
