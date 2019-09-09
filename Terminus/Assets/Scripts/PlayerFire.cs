395ebbd5edb671da3817882c805cb0e6cf61a09e
Sam Layton <slayton@uccs.edu>
2019-09-09 16:02:26 -0600

    PC registers fire input on first frame button is held down

--- Terminus/Assets/Scripts/PlayerFire.cs
+++ Terminus/Assets/Scripts/PlayerFire.cs
@@ -10,6 +10,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerFire : MonoBehaviour
 {
+    // private variables
+    bool firedLastFrame = false;        // flag to track whether player has fired since last frame (used for semi-automatice weapons)
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,19 @@ public class PlayerFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // if player character registers fire input (mouse 0 by default)
+        if (Input.GetAxis("Fire") != 0)
+        {
+            // if player hasn't fired last frame
+            if (!firedLastFrame)
+            {
+                firedLastFrame = true;
+                Debug.Log("Fire");
+            }
+        }
+        // otherwise (no fire input registered)
+        else
+            // reset fired last frame flag
+            firedLastFrame = false;
     }
 }
