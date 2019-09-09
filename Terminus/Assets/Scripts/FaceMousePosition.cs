6731b6326ce01daa09436cf7f1b59e601fc801b3
Sam Layton <slayton@uccs.edu>
2019-09-09 15:28:20 -0600

    Mouse rotation lerps object to right angle rather than instantaneously rotating it

--- Terminus/Assets/Scripts/FaceMousePosition.cs
+++ Terminus/Assets/Scripts/FaceMousePosition.cs
@@ -7,14 +7,20 @@ using UnityEngine;
 /// </summary>
 public class FaceMousePosition : MonoBehaviour
 {
+    // public variables
+    public float rotationSpeed = 10f;   // rate at which object turns to face mouse position
+
     // Update is called once per frame
     void Update()
     {
-        // rotate object to face mouse position
+        // find angle between object and mouse position
         Vector2 PCToMouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)) 
             - transform.position;
         float angleToMouse = Mathf.Atan2(PCToMouse.y, PCToMouse.x) * Mathf.Rad2Deg;
-        transform.Rotate((Vector3.forward * angleToMouse) - transform.rotation.eulerAngles);
 
+        // turn object to face user's mouse
+        Quaternion targetOrientation = new Quaternion();
+        targetOrientation.eulerAngles = new Vector3(0, 0, angleToMouse);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetOrientation, rotationSpeed);
     }
 }
