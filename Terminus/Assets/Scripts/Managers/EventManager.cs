0c703d415ec357768ce88bdf4f63efa8ecc6c92f
Sam Layton <slayton@uccs.edu>
2019-09-09 23:24:09 -0600

    Added event manager. Oxygen controller adds self as invoker of update O2 display event.

---
+++ Terminus/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Facilitates adding invokers and listeners to various in-game events
+/// </summary>
+public static class EventManager
+{
+    #region Update O2 Gauge
+
+    // declare lists to hold invokers and listeners to update O2 display event
+    static List<OxygenControl> updateO2Invokers = new List<OxygenControl>();
+    static List<UnityAction<float>> updateO2Listeners = new List<UnityAction<float>>();
+
+    // Adds given oxygen controller as invoker of update O2 event
+    public static void AddUpdateO2Invoker(OxygenControl invoker)
+    {
+        // adds invoker to list and adds all listeners to this invoker
+        updateO2Invokers.Add(invoker);
+        foreach (UnityAction<float> listener in updateO2Listeners)
+            invoker.AddUpdateO2Listener(listener);
+    }
+
+    // Adds given method as listener to update O2 event
+    public static void AddUpdateO2Listener(UnityAction<float> listener)
+    {
+        // adds listener to list and to all invokers of event
+        updateO2Listeners.Add(listener);
+        foreach (OxygenControl invoker in updateO2Invokers)
+            invoker.AddUpdateO2Listener(listener);
+    }
+
+    #endregion
+}
--- Terminus/Assets/Scripts/Player Status and Control/OxygenControl.cs
+++ Terminus/Assets/Scripts/Player Status and Control/OxygenControl.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 /// <summary>
 /// Manages oxygen depletion and re-gain of agent
 /// </summary>
 public class OxygenControl : MonoBehaviour
 {
-    // public variables
-    public float oxygenDepletionRate = 1f;  // percent of oxygen used per second
-
     // private variables
     int maxOxygen = 100;                    // total capacity of agent's oxygen tank
     float currOxygen = 0;                   // remaining percentage of agent's oxygen tank
 
+    // public variables
+    public float oxygenDepletionRate = 1f;  // percent of oxygen used per second
+
+    // event support
+    UpdateO2DisplayEvent updateO2Event;    // event invoked to update player's oxygen on UI
+
     // Start is called before the first frame update
     void Start()
     {
+        // add self as invoker to update O2 display event
+        updateO2Event = new UpdateO2DisplayEvent();
+        EventManager.AddUpdateO2Invoker(this);
+
         // "fill" agent's oxygen tank to capacity
         currOxygen = maxOxygen;
     }
@@ -24,7 +32,18 @@ public class OxygenControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // reduce remaining oxygen, clamping above 0
+        // reduce remaining oxygen, rounding up to whole number above 0
         currOxygen = Mathf.Max(0, currOxygen - (oxygenDepletionRate * Time.deltaTime));
+
+        // TODO: Update oxygen display
+    }
+
+    /// <summary>
+    /// Adds given listener to Update O2 display event
+    /// </summary>
+    /// <param name="newListener">new listener for event</param>
+    public void AddUpdateO2Listener(UnityAction<float> newListener)
+    {
+        updateO2Event.AddListener(newListener);
     }
 }
