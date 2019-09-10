1c0ffb7db6cfd6e61e6acd163b182f96d61fb9ef
Sam Layton <slayton@uccs.edu>
2019-09-09 22:51:13 -0600

    Player's oxygen depletes at user-defined rate

--- Terminus/Assets/Scripts/Player Status and Control/OxygenControl.cs
+++ Terminus/Assets/Scripts/Player Status and Control/OxygenControl.cs
@@ -12,17 +12,20 @@ public class OxygenControl : MonoBehaviour
 
     // private variables
     int maxOxygen = 100;                    // total capacity of agent's oxygen tank
-    float currOxygen = 0;
+    float currOxygen = 0;                   // remaining percentage of agent's oxygen tank
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // "fill" agent's oxygen tank to capacity
+        currOxygen = maxOxygen;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // reduce remaining oxygen, clamping above 0
+        currOxygen = Mathf.Max(0, currOxygen - (oxygenDepletionRate * Time.deltaTime));
+        Debug.Log(currOxygen);
     }
 }
