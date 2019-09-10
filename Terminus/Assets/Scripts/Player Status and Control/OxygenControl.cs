39049ea1b043182b28bde5f9c63522b66196b864
Sam Layton <slayton@uccs.edu>
2019-09-09 22:45:46 -0600

    Added script to control player's oxygen depletion and regain

---
+++ Terminus/Assets/Scripts/Player Status and Control/OxygenControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages oxygen depletion and re-gain of agent
+/// </summary>
+public class OxygenControl : MonoBehaviour
+{
+    // public variables
+    public float oxygenDepletionRate = 1f;  // percent of oxygen used per second
+
+    // private variables
+    int maxOxygen = 100;                    // total capacity of agent's oxygen tank
+    float currOxygen = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
