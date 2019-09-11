3c168b482b4142c6a0456170d233544b1951a35c
Sam Layton <slayton@uccs.edu>
2019-09-11 14:10:32 -0600

    Created event to deduct player's O2

---
+++ Terminus/Assets/Scripts/Events/DeductPlayerO2Event.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event invoked to deduct float amount of O2 from player's tank
+/// </summary>
+public class DeductPlayerO2Event : UnityEvent<float>
+{ 
+}
