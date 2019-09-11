8b956a960aad5998fe3a76c3994d9e5b90f5c102
Sam Layton <slayton@uccs.edu>
2019-09-11 16:29:40 -0600

    Chaser agent can determine whether it sees its target

--- Terminus/Assets/Scripts/Agent Behaviors/PursueAndAttack.cs
+++ Terminus/Assets/Scripts/Agent Behaviors/PursueAndAttack.cs
@@ -6,20 +6,79 @@ using UnityEngine;
 /// Enemy agent behavior of pursuing player once within
 /// sight range and attacking within melee distance.
 /// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
 public class PursueAndAttack : MonoBehaviour
 {
+    // state enumeration
+    public enum ChaseStates
+    {
+        Idle,
+        Pursue,
+        AttackAndWait
+    }
+
     // public variables
-    public ChaserStates startingState = ChaserStates.Idle;  // state which pursuing agent starts in (typically Idle)
+    public ChaseStates startingState = ChaseStates.Idle;    // state which pursuing agent starts in (typically Idle)
     public Transform targetTransform;                       // transform of target to pursue (typically player)
+    public float speed = 5f;                                // magnitude of agent's velocity
+    public float sightRange = 30f;                          // max distance agent can see target without objects obstructing its view
 
     // private variables
-    ChaserStates currState;
+    ChaseStates currState;
+
+    #region State Machine
+
+    /// <summary>
+    /// Updates agent as it waits idly
+    /// </summary>
+    void UpdateIdle()
+    {
+        Debug.Log(CanSeeTarget());
+    }
+
+    /// <summary>
+    /// Updates agent as it pursues its target
+    /// </summary>
+    void UpdatePursue()
+    {
+        Debug.Log("Pursue");
+    }
+
+    /// <summary>
+    /// Updates agent as it attacks enemy and then waits cooldown time
+    /// </summary>
+    void UpdateAttackAndWait()
+    {
+        Debug.Log("Attack and Wait");
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Determines whether agent's target is within
+    /// sight range and has line of sight
+    /// </summary>
+    /// <returns>whether agent 'sees' target</returns>
+    bool CanSeeTarget()
+    {
+        // Shoot raycast towards target, returning whether it hit
+        return (Physics2D.Raycast(transform.position, targetTransform.position - transform.position, sightRange).transform == targetTransform);
+    }
+
+    #endregion
+
+    #region Unity Methods
 
     /// <summary>
     /// Used for initialization
     /// </summary>
     void Awake()
     {
+        // initialize agent's state
+        currState = startingState;
+
         // if target wasn't set before launch
         if (targetTransform == null)
         {
@@ -40,6 +99,23 @@ public class PursueAndAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // call state-appropriate Update behavior
+        switch (currState)
+        {
+            case ChaseStates.Idle:
+                UpdateIdle();
+                break;
+            case ChaseStates.Pursue:
+                UpdatePursue();
+                break;
+            case ChaseStates.AttackAndWait:
+                UpdateAttackAndWait();
+                break;
+            default:
+                break;
+        }
     }
+
+    #endregion
+
 }
