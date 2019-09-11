2ff0f215ef353a51c95ad9af30c9a07b47f0437c
Sam Layton <slayton@uccs.edu>
2019-09-11 15:48:13 -0600

    Created enumeration of Chaser state

---
+++ Terminus/Assets/Scripts/Enumerations/ChaserStates.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An enumeration of states within the
+/// Chaser finite state machine.
+/// </summary>
+public enum ChaserStates
+{
+    Idle,
+    Pursue,
+    AttackAndWait
+}
