e20c3805b13a9b3e663855577c3fc30138c56080
Sam Layton <slayton@uccs.edu>
2019-09-11 20:38:18 -0600

    Added projectile component to carry damage info (and possibly other properties in future)

---
+++ Terminus/Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls damage-dealing beheviors internal to projectile
+/// </summary>
+public class Projectile : MonoBehaviour
+{
+    // private variables
+    [SerializeField] float damage = 5f;     // damage dealt by projectile on collision
+
+    /// <summary>
+    /// Provides public read-access to damage dealt 
+    /// by projectile on collision
+    /// </summary>
+    public float Damage
+    {
+        get { return damage; }
+    }
+}
