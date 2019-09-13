548396f73149b66d88f9730099851b8a8ff92001
Sam Layton <slayton@uccs.edu>
2019-09-13 10:31:02 -0600

    User can close application from main menu

---
+++ Terminus/Assets/Scripts/UI Control/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Closes application when user clicks object.
+/// </summary>
+public class QuitOnClick : MonoBehaviour
+{
+    /// <summary>
+    /// Closes application on click
+    /// </summary>
+    public void HandleClickEvent()
+    {
+        Debug.Log("Application closed by user after running for " + Time.time + " seconds.");
+        Application.Quit();
+    }
+}
