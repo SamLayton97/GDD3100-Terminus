ddee7265dd422b986425667b55415eb89290931f
Sam Layton <slayton@uccs.edu>
2019-09-12 19:58:27 -0600

    Added abstract base class for any object facilitating transitions to single scene

---
+++ Terminus/Assets/Scripts/Scene Control/SceneTransitioner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Base class of objects which facilitate transitions between scenes
+/// (typically managers);
+/// </summary>
+public abstract class SceneTransitioner : MonoBehaviour
+{
+    // public variables
+    public Scene transitionTo;      // scene which object can transition to
+
+    /// <summary>
+    /// Provides read-access to name of scene object can load.
+    /// Accessible to children of this class.
+    /// </summary>
+    protected string TransitionTo
+    {
+        get { return transitionTo.name; }
+    }
+}
