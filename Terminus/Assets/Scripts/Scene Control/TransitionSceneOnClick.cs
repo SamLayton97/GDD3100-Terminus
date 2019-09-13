441d339e331015a7d31962d2976a7137978ad1f7
Sam Layton <slayton@uccs.edu>
2019-09-13 08:59:38 -0600

    Added framework for scene-transitioning UI buttons

---
+++ Terminus/Assets/Scripts/Scene Control/TransitionSceneOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Elects to transition to specified scene when user clicks object.
+/// Typically used by scene-transitioning UI buttons.
+/// </summary>
+public class TransitionSceneOnClick : SceneTransitioner
+{
+    /// <summary>
+    /// Invokes transition scene event when clicked
+    /// </summary>
+    public void HandleClickEvent()
+    {
+        transitionSceneEvent.Invoke(transitionTo);
+    }
+}
