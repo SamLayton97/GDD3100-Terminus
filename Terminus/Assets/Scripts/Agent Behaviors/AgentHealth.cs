e727bdb28bad9b8a9f9af9cf86aebfde4c2f83cf
Sam Layton <slayton@uccs.edu>
2019-09-19 23:28:24 -0600

    When chaser agent dies, it becomes inactive

--- Terminus/Assets/Scripts/Agent Behaviors/AgentHealth.cs
+++ Terminus/Assets/Scripts/Agent Behaviors/AgentHealth.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 /// <summary>
 /// Controls health status and death of enemy agent
 /// </summary>
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class AgentHealth : MonoBehaviour
 {
     // public variables
-    public float maxHealth = 15f;     // starting health of enemy agent
+    public float maxHealth = 15f;       // starting health of enemy agent
+    public Color deathColor;            // color to darken agent to on death
 
     // private variables
-    float currHealth;       // current health of agent
+    bool isDead = false;                // flag determining whether agent has died (controls 'soft-disabling' of agent)
+    float currHealth;                   // current health of agent
+    SpriteRenderer mySpriteRenderer;    // agent's sprite renderer component (used to set color of sprite)
+    Animator myAnimator;                // animator used to play agent's death animation (if it has one)
+    O2Remover myBehavior;               // component controlling agent's hostile behavior
 
     /// <summary>
     /// Used for internal initialization
     /// </summary>
     void Awake()
     {
+        // retrieve necessary components
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
+        myBehavior = GetComponent<O2Remover>();
+
         // initialize health
         currHealth = maxHealth;
     }
@@ -33,7 +45,22 @@ public class AgentHealth : MonoBehaviour
         {
             // deduct health and destroy gameobject if necessary
             currHealth -= collision.gameObject.GetComponent<Projectile>().Damage;
-            if (currHealth <= 0) Destroy(gameObject);
+            if (currHealth <= 0) HandleAgentDeath();
         }
+    }
+
+    /// <summary>
+    /// Handles all necessary processes related to handling agent's death
+    /// </summary>
+    void HandleAgentDeath()
+    {
+        // set animation triggers
+        myAnimator.SetTrigger("OnDeathTrigger");
+
+        // soft-disable agent
+        myBehavior.enabled = false;
+        mySpriteRenderer.color = deathColor;
+        gameObject.layer = LayerMask.NameToLayer("Environment");
+        mySpriteRenderer.sortingLayerName = "MiscellaneousObjects";
     }
 }
