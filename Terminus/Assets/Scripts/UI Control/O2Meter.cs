c571c03b5baa9b0c93b9bc9544e58355f323f4cc
Sam Layton <slayton@uccs.edu>
2019-09-20 16:22:58 -0600

    Created O2 meter script to control O2 side of UI

---
+++ Terminus/Assets/Scripts/UI Control/O2Meter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Graphically displays player's remaining oxygen by scaling a meter
+/// </summary>
+public class O2Meter : MonoBehaviour
+{
+    // public variables
+    public Image guageMeter;       // UI image to represent player's remaining O2
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // if not set prior to launch, retrieve O2 meter image from child
+        if (guageMeter == null)
+            guageMeter = GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
