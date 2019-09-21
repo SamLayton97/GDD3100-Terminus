b3a2c68905a56c04b08df057b0437f3011e082aa
Sam Layton <slayton@uccs.edu>
2019-09-21 01:05:13 -0600

    Created and applied empty PauseControl.cs script

---
+++ Terminus/Assets/Scripts/UI Control/PauseControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Pauses/unpauses game on user input
+/// </summary>
+public class PauseControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
