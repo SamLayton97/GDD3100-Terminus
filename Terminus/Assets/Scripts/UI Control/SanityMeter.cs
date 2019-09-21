c1f1879ee54182947c5a598c7cbe7a23fb6dc5b9
Sam Layton <slayton@uccs.edu>
2019-09-20 20:22:29 -0600

    Created script to listen for and handle invocations to update sanity meter

---
+++ Terminus/Assets/Scripts/UI Control/SanityMeter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Graphically displays player's sanity by scaling a meter
+/// </summary>
+public class SanityMeter : MeterScaler
+{
+    /// <summary>
+    /// Called before the first frame of Update
+    /// </summary>
+    protected override void Start()
+    {
+        // add self as listener to update sanity display event
+        EventManager.AddUpdateSanityListener(UpdateDisplay);
+    }
+}
