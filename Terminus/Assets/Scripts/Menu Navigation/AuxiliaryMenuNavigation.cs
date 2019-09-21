b4296293c0e4763aaaa45a33778b535f900a02e2
Sam Layton <slayton@uccs.edu>
2019-09-21 15:18:31 -0600

    Pressing "return" in aux menu brings player to specified scene (most commonly title scene)

---
+++ Terminus/Assets/Scripts/Menu Navigation/AuxiliaryMenuNavigation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls navigation between scenes from
+/// the auxiliary menus such as the Controls
+/// and Credits scenes.
+/// </summary>
+public class AuxiliaryMenuNavigation : SceneTransitioner
+{
+    /// <summary>
+    /// Handles player pressing the "Return" button
+    /// </summary>
+    public void ReturnButtonOnClick()
+    {
+        // transition to menu before this (likely Title scene)
+        transitionSceneEvent.Invoke(transitionTo[0]);
+    }
+}
