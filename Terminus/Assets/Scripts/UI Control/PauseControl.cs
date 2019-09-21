db7769c0ec7a27aea13000bfb65ac38a44bd39c9
Sam Layton <slayton@uccs.edu>
2019-09-21 11:47:50 -0600

    User can pause game using ESC

--- Terminus/Assets/Scripts/UI Control/PauseControl.cs
+++ Terminus/Assets/Scripts/UI Control/PauseControl.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 /// </summary>
 public class PauseControl : MonoBehaviour
 {
+    // public variables
+    public GameObject darkenGameOnPause;                // semi-transparent panel covering game when paused
+    public GameObject pauseMenu;                        // pop-up pause menu
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,15 @@ public class PauseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if user attempts to pause game and game is not already paused
+        if (Input.GetAxis("Pause") != 0 && Time.timeScale != 0)
+        {
+            // freeze game
+            Time.timeScale = 0;
+
+            // enable pause menu components
+            darkenGameOnPause.SetActive(true);
+            pauseMenu.SetActive(true);
+        }
     }
 }
