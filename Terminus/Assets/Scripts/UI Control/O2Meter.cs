70f9898b09533eb56adc17e08e8775098717326d
Sam Layton <slayton@uccs.edu>
2019-09-20 20:19:18 -0600

    Applied inheritence to O2 meter and confirmed identical functionality

--- Terminus/Assets/Scripts/UI Control/O2Meter.cs
+++ Terminus/Assets/Scripts/UI Control/O2Meter.cs
@@ -6,36 +6,14 @@ using UnityEngine.UI;
 /// <summary>
 /// Graphically displays player's remaining oxygen by scaling a meter
 /// </summary>
-public class O2Meter : MonoBehaviour
+public class O2Meter : MeterScaler
 {
-    // public variables
-    public RectTransform guageMeter;    // RectTransform component of meter representing player's remaining O2
-
-    /// <summary>
-    /// Used for initialization
-    /// </summary>
-    void Awake()
-    {
-        // if not set prior to launch, retrieve rect transform component
-        if (guageMeter == null)
-            guageMeter = GetComponent<RectTransform>();
-    }
-
     /// <summary>
     /// Called before first frame of Update
     /// </summary>
-    void Start()
+    protected override void Start()
     {
         // add self as listener to Update O2 display event
-        EventManager.AddUpdateO2Listener(UpdateO2Display);
-    }
-
-    /// <summary>
-    /// Updates O2 meter by scaling it
-    /// </summary>
-    /// <param name="remainingOxygen">percentage of oxygen left in player's tank</param>
-    void UpdateO2Display(float remainingOxygen)
-    {
-        guageMeter.localScale = new Vector2(Mathf.Clamp01(remainingOxygen / 100), guageMeter.localScale.y);
+        EventManager.AddUpdateO2Listener(UpdateDisplay);
     }
 }
