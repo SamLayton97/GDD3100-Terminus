94cb040c7c4ab4f2b1d9e88d8d39916cfe1c7e6a
Sam Layton <slayton@uccs.edu>
2019-09-21 13:35:35 -0600

    Added method stubs for main menu navigation script

---
+++ Terminus/Assets/Scripts/Menu Navigation/MainMenuNavigation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls navigation between scenes from
+/// the main menu/title screen
+/// </summary>
+public class MainMenuNavigation : SceneTransitioner
+{
+    /// <summary>
+    /// Handles player pressing the "Play" button
+    /// </summary>
+    public void PlayButtonOnClick()
+    {
+
+    }
+
+    /// <summary>
+    /// Handles player pressing the "Help" button
+    /// </summary>
+    public void HelpButtonOnClick()
+    {
+
+    }
+
+    /// <summary>
+    /// Handles player pressing the "Credits" button
+    /// </summary>
+    public void CreditsButtonOnClick()
+    {
+
+    }
+
+    /// <summary>
+    /// Handles player pressing the "Quit" button
+    /// </summary>
+    public void QuitButtonOnClick()
+    {
+
+    }
+}
