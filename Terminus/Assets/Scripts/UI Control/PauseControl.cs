5244cbfc9936284bcfa558f91f30434c0f52a2ee
Sam Layton <slayton@uccs.edu>
2019-09-21 12:02:23 -0600

    User can unpause game by clicking resume button

--- Terminus/Assets/Scripts/UI Control/PauseControl.cs
+++ Terminus/Assets/Scripts/UI Control/PauseControl.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class PauseControl : MonoBehaviour
 {
     // public variables
+    public KeyCode pauseKey = KeyCode.Escape;           // key to pause/unpause game
     public GameObject darkenGameOnPause;                // semi-transparent panel covering game when paused
     public GameObject pauseMenu;                        // pop-up pause menu
 
@@ -15,7 +16,7 @@ public class PauseControl : MonoBehaviour
     void Update()
     {
         // if user attempts to pause game and game is not already paused
-        if (Input.GetAxis("Pause") != 0 && Time.timeScale != 0)
+        if (Input.GetKeyDown(pauseKey) && Time.timeScale != 0)
         {
             // freeze game
             Time.timeScale = 0;
@@ -31,12 +32,15 @@ public class PauseControl : MonoBehaviour
     /// </summary>
     public void HandleResumeOnClick()
     {
+        Debug.Log("Getting called");
+
         // disable pause menu components
         darkenGameOnPause.SetActive(false);
         pauseMenu.SetActive(false);
 
         // unfreeze game
         Time.timeScale = 1;
+        Debug.Log(Time.timeScale);
     }
 
     /// <summary>
@@ -45,7 +49,7 @@ public class PauseControl : MonoBehaviour
     public void HandleControlsOnClick()
     {
         // TODO: remove pause menu and display control scheme
-
+        Debug.Log("Display Controls");
     }
 
     /// <summary>
@@ -54,6 +58,6 @@ public class PauseControl : MonoBehaviour
     public void HandleExitOnClick()
     {
         // TODO: return to main menu
-
+        Debug.Log("Return to main menu");
     }
 }
