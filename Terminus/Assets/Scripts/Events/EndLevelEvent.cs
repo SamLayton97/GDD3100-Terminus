a57ff71de10521cc6bc1579ed3fa9cc646f7214d
Sam Layton <slayton@uccs.edu>
2019-09-22 12:59:53 -0600

    Added event to facilitate ending a level

---
+++ Terminus/Assets/Scripts/Events/EndLevelEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event used to handle ending level on player success or failure.
+/// Holds success flag (bool) and remaining sanity (float) on level end.
+/// </summary>
+public class EndLevelEvent : UnityEvent<bool, float>
+{
+}
