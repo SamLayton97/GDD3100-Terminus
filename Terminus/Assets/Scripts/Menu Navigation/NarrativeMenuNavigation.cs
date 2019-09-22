9008f72ff643226020cdcf240717041d572ce8bb
Sam Layton <slayton@uccs.edu>
2019-09-21 21:01:04 -0600

    Added method stubs to narrative menu scene navigation script

---
+++ Terminus/Assets/Scripts/Menu Navigation/NarrativeMenuNavigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls navigation between scenes from
+/// the narrative-driven menus, typically moving
+/// forward to something or back.
+/// </summary>
+public class NarrativeMenuNavigation : MonoBehaviour
+{
+    /// <summary>
+    /// Handles when player clicks the "Return" button
+    /// </summary>
+    public void ReturnButtonOnClick()
+    {
+
+    }
+
+    /// <summary>
+    /// Handles when player clicks the "Proceed" button
+    /// </summary>
+    public void ProceedButtonOnClick()
+    {
+
+    }
+}
