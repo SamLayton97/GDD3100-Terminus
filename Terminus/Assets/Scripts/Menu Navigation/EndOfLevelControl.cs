4b6cc44e516e989e406887d45a0851a0f4448e3f
Sam Layton <slayton@uccs.edu>
2019-09-22 12:47:43 -0600

    End of level screen can reload scene and return to main menu

--- Terminus/Assets/Scripts/Menu Navigation/EndOfLevelControl.cs
+++ Terminus/Assets/Scripts/Menu Navigation/EndOfLevelControl.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Pauses game when user ends level either through death or
 /// meeting their objective. Also, handles click events for
 /// its buttons on popup.
 /// </summary>
-public class EndOfLevelControl : MonoBehaviour
+public class EndOfLevelControl : SceneTransitioner
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public variables
+    public GameObject darkenGameOnPause;                // semi-transparent panel covering game when paused
+    public GameObject endOfLevelMenu;                   // menu shown when user ends a level
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Handles when user clicks retry button
+    /// </summary>
+    public void HandleRetryOnClick()
+    {
+        // reload current scene
+        transitionSceneEvent.Invoke(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Handles when user clicks proceed button
+    /// </summary>
+    public void HandleProceedOnClick()
+    {
+        // move forward to next pre-defined scene
+        transitionSceneEvent.Invoke(transitionTo[0]);
     }
 }
