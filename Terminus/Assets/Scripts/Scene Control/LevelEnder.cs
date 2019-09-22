ca867047839461ecd5af2ce875e63b8a81bd81e9
Sam Layton <slayton@uccs.edu>
2019-09-22 13:07:32 -0600

    Created level ender base class

---
+++ Terminus/Assets/Scripts/Scene Control/LevelEnder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Abstract parent class of all objects capable of
+/// ending a level in success or failure.
+/// </summary>
+public abstract class LevelEnder : MonoBehaviour
+{
+    // event support
+    EndLevelEvent endLevelEvent;
+
+    /// <summary>
+    /// Called before first frame Update
+    /// </summary>
+    protected void Start()
+    {
+        // TODO: add self as invoker of End Level event
+        endLevelEvent = new EndLevelEvent();
+
+    }
+
+    /// <summary>
+    /// Adds given listener to End Level event
+    /// </summary>
+    /// <param name="newListener">new method listening for event</param>
+    public void AddEndLevelListener(UnityAction<bool, float> newListener)
+    {
+        endLevelEvent.AddListener(newListener);
+    }
+}
