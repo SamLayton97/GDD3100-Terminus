eb0c5a7dddfb748f2bb34b2e6b423360b01d445e
Sam Layton <slayton@uccs.edu>
2019-09-28 15:58:19 -0600

    Main menu buttons play highlight sound

--- Terminus/Assets/Scripts/UI Control/PlaySoundOnMouseOver.cs
+++ Terminus/Assets/Scripts/UI Control/PlaySoundOnMouseOver.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 /// <summary>
-/// Plays a specified sound when user mouses over object.
-/// Typically used by interactable UI elements such as buttons.
+/// Plays a specified sound when user mouses over a UI element.
+/// Typically used by interactable elements such as buttons.
 /// </summary>
+[RequireComponent(typeof(EventTrigger))]
 public class PlaySoundOnMouseOver : MonoBehaviour
 {
     // public variables
     public AudioClipNames myMouseOverSound =    // sound played when user mouses over object
         AudioClipNames.UI_buttonHighlight;
+
+    /// <summary>
+    /// Play sound when user mouses over object
+    /// </summary>
+    public void HandlePointerEnter()
+    {
+        AudioManager.Play(myMouseOverSound, true);
+    }
 }
