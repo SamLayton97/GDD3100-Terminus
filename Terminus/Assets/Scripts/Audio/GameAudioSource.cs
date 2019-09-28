847356859184948946bf3e3bbd2cc7a7deb7f7e9
Sam Layton <slayton@uccs.edu>
2019-09-28 10:37:44 -0600

    Added script to handle game audio source singleton

---
+++ Terminus/Assets/Scripts/Audio/GameAudioSource.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Audio source to play all in-game sound effects
+/// </summary>
+public class GameAudioSource : MonoBehaviour
+{
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // TODO: keep single game audio source object for entire game
+
+    }
+}
