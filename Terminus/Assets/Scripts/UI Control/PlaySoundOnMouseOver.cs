4721b747a097d9fd080bae2bb58ce30bc8ba98a5
Sam Layton <slayton@uccs.edu>
2019-09-28 15:45:32 -0600

    Created script to play sound when user first mouses over UI element

---
+++ Terminus/Assets/Scripts/UI Control/PlaySoundOnMouseOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Plays a specified sound when user mouses over object.
+/// Typically used by interactable UI elements such as buttons.
+/// </summary>
+public class PlaySoundOnMouseOver : MonoBehaviour
+{
+    // public variables
+    public AudioClipNames myMouseOverSound =    // sound played when user mouses over object
+        AudioClipNames.UI_buttonHighlight;
+
+    /// <summary>
+    /// Plays sound when user first mouses over object
+    /// </summary>
+    void OnMouseOver()
+    {
+        AudioManager.Play(myMouseOverSound, true);
+    }
+}
