b61a565079db2010516d5c5b4d7505d931f1b06a
Sam Layton <slayton@uccs.edu>
2019-09-28 10:34:57 -0600

    Added enumeration of audio clips

---
+++ Terminus/Assets/Scripts/Audio/AudioClips.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An enumeration of audio clips to play
+/// </summary>
+public enum AudioClips
+{
+    // Menu Sounds
+    menu_ButtonClick,
+    menu_ButtonHighlight,
+
+    // Player Sounds
+    player_takeDamage,
+    player_collectOxygen,
+    player_death,
+    player_shootSingle,
+    player_shootShotgun,
+    player_shootPhoton,
+    player_shootBioshot,
+
+    // Agent Sounds
+    agent_chaserAttack,
+    agent_chaserDeath,
+
+    // Environment Sounds
+    env_airlockClosing
+}
