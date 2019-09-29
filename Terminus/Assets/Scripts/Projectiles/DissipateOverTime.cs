e975d620db419b59be727bddfb0c6ac886296d1a
Sam Layton <slayton@uccs.edu>
2019-09-28 20:57:37 -0600

    Added script to handle photon dissapation

---
+++ Terminus/Assets/Scripts/Projectiles/DissipateOverTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Causes object to fade away and disappear over time.
+/// Typically used by photon projectiles.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer)]
+public class DissipateOverTime : MonoBehaviour
+{
+    // public variables
+    public float timeToDisappear = 6f;          // time it takes for object to fully disappear
+
+    // private variables
+    SpriteRenderer mySpriteRenderer;            // object's sprite renderer component (used to control opacity)
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // retrieve necessary components
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
