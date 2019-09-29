c3bf78b70b9f47a4e0453cb4be49648ace90d7ee
Sam Layton <slayton@uccs.edu>
2019-09-29 17:09:16 -0600

    Created new event to handle updates to weapons' current ammo on UI

---
+++ Terminus/Assets/Scripts/Events/UpdateAmmoUIEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Updates weapon UI to reflect remaining ammo of a given weapon
+/// </summary>
+public class UpdateAmmoUIEvent : UnityEvent<WeaponType, float>
+{
+}
