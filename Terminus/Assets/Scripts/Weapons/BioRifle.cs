dd93dbea7b284c85bf36a81c160e9a933d06989b
Sam Layton <slayton@uccs.edu>
2019-09-28 21:29:43 -0600

    Added child BioRifle object script

---
+++ Terminus/Assets/Scripts/Weapons/BioRifle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Semi-automatic weapon which fires a single
+/// "corrupted"/poisoned bullet each time it registers
+/// fire input
+/// </summary>
+public class BioRifle : Weapon
+{
+    /// <summary>
+    /// Fires a bio-projectile, applying reactionary force to user
+    /// and reducing their sanity
+    /// </summary>
+    /// <param name="firedLastFrame">whether user of weapon fired on last frame</param>
+    public override void RegisterInput(bool firedLastFrame)
+    {
+        throw new System.NotImplementedException();
+    }
+}
