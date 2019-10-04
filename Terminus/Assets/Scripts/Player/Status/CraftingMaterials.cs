d73dd0e518657b00a0a5d3106ee9b19510c5a460
Sam Layton <slayton@uccs.edu>
2019-10-04 17:37:11 -0600

    Added small enumeration of crafting materials (correspond to pickups player can find in level

---
+++ Terminus/Assets/Scripts/Player/Status/CraftingMaterials.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An enumeration of materials player can craft with
+/// </summary>
+public enum CraftingMaterials
+{
+    casing,
+    powder,
+    biomass
+}
