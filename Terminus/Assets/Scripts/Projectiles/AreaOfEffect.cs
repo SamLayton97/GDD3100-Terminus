3e6048cbfa913e760615e0505e995fde30344a90
Sam Layton <slayton@uccs.edu>
2019-10-04 10:23:43 -0600

    Explosion destroys itself when its animation ends

---
+++ Terminus/Assets/Scripts/Projectiles/AreaOfEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls area-of-effect done to enemies. Typically used by 
+/// explosions created by projectiles who burst on contact/over time.
+/// </summary>
+[RequireComponent(typeof(CircleCollider2D))]
+public class AreaOfEffect : MonoBehaviour
+{
+    // private variables
+    CircleCollider2D myTriggerCollider;
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // retrieve collider and ensure that it is a trigger
+        myTriggerCollider = GetComponent<CircleCollider2D>();
+        myTriggerCollider.isTrigger = true;
+    }
+
+    /// <summary>
+    /// Destroys explosion when it's animation ends
+    /// </summary>
+    public void DestroyOnAnimationEnd()
+    {
+        Destroy(gameObject);
+    }
+}
