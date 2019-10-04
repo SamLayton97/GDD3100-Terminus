7f14fc5579cc46a12bcc30d77cf9f6c7fdfd9614
Sam Layton <slayton@uccs.edu>
2019-10-04 17:48:51 -0600

    Added script which will control basic material pickup behaviors

---
+++ Terminus/Assets/Scripts/Environment/AddCraftingMaterialOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Adds crafting materials of specific type to player's
+/// inventory on collision
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+public class AddCraftingMaterialOnCollision : MonoBehaviour
+{
+    // serialized variables
+    [SerializeField] CraftingMaterials materialToAdd =  // type of crafting material to give to player on collision
+        CraftingMaterials.biomass;
+    [Range(1, 10)]
+    [SerializeField] int amountToAdd = 1;               // amount of specific material to add to player's inventory
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
