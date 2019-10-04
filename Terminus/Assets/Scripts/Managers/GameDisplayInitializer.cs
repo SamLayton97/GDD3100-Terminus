79f7879062861232d6a56b344a1d4e372ece702e
Sam Layton <slayton@uccs.edu>
2019-10-03 22:11:18 -0600

    Created display initializer script which handles initializing aspects relevant to user's display settings

---
+++ Terminus/Assets/Scripts/Managers/GameDisplayInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Initializes aspects of game relevant to user's display.
+/// This happens once when game loads.
+/// </summary>
+public class GameDisplayInitializer : MonoBehaviour
+{
+    // private variables
+    bool isInitialized = false;
+
+    // serialized fields
+    [Range(30, 300)]
+    [SerializeField]
+    int targetFrameRate = 60;           // max framerate game will run at
+
+    /// <summary>
+    /// Used for early display initialization
+    /// </summary>
+    void Awake()
+    {
+        // TODO: lock framerate
+
+    }
+
+    void Update()
+    {
+        Debug.Log("Target: " + Application.targetFrameRate + " Actual: " + (1.0f / Time.deltaTime));
+    }
+}
