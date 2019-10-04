536bb1732808665c2e6a2ebb38556d8cdeac5116
Sam Layton <slayton@uccs.edu>
2019-10-04 10:01:41 -0600

    Added script to facilitate creation of projectile area of effect stuff.

---
+++ Terminus/Assets/Scripts/Projectiles/ExplodeOnContact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Causes projectile to explode on collision. Typically used
+/// by projectiles who have area of effect (e.g., bio projectiles).
+/// </summary>
+public class ExplodeOnContact : MonoBehaviour
+{
+    // public variables
+    public GameObject myExplosion;
+
+    /// <summary>
+    /// Called when object's collider meets with another
+    /// </summary>
+    /// <param name="collision">collision data</param>
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // TODO: create explosion
+        Debug.Log("Boom!");
+
+        // destroy self
+        Destroy(gameObject);
+    }
+}
