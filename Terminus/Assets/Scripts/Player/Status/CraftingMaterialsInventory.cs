ff4dbe0940fe99774004e8c7fa6d3f0f03773edf
Sam Layton <slayton@uccs.edu>
2019-10-05 09:31:28 -0600

    Moved enumeration of crafting materials to player's inventory script (for uniformity)

---
+++ Terminus/Assets/Scripts/Player/Status/CraftingMaterialsInventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An enumeration of materials player can craft with
+/// </summary>
+public enum CraftingMaterials
+{
+    casing,
+    powder,
+    biomass
+}
+
+/// <summary>
+/// Holds amount of crafting materials player has collected
+/// (per type), allowing them to craft usable items (e.g.,
+/// bio-rifle ammo).
+/// </summary>
+public class CraftingMaterialsInventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
