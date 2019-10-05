026afa84d318fb0a30707a2c06868cc3ac641cac
Sam Layton <slayton@uccs.edu>
2019-10-04 18:42:23 -0600

    biomass pickup randomizes its sprite on initialization

---
+++ Terminus/Assets/Scripts/Environment/RandomizeInitialSprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Randomizes initial sprite used by an object
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomizeInitialSprite : MonoBehaviour
+{
+    // serialized variables
+    [SerializeField] Sprite[] spritesToChoose;      // collection of sprites to choose from on initialization
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // pull random sprite and set it as object's sprite
+        GetComponent<SpriteRenderer>().sprite = spritesToChoose[Random.Range(0, spritesToChoose.Length)];
+    }
+}
