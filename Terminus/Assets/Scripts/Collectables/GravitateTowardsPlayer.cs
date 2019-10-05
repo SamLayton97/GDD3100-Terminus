a0c6b1b9def4ca03e680959eb4c846abc547379b
Sam Layton <slayton@uccs.edu>
2019-10-04 20:47:46 -0600

    biomass pickup gravitates towards player once within range

--- Terminus/Assets/Scripts/Collectables/GravitateTowardsPlayer.cs
+++ Terminus/Assets/Scripts/Collectables/GravitateTowardsPlayer.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Moves object towards player once within specified range
+/// </summary>
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class GravitateTowardsPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // private variables
+    Rigidbody2D myRigidbody2D;          // object's rigidbody2d component (used to change object's velocity)
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
     {
-        
+        // retrieve necessary components
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        // ensure object's circle collider is a trigger
+        GetComponent<CircleCollider2D>().isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Called each frame an object is within trigger collider
+    /// </summary>
+    /// <param name="collider">collision data</param>
+    void OnTriggerStay2D(Collider2D collider)
     {
-        
+        // ensure object is on the player layer and collider is player's body
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player") &&
+            !collider.isTrigger)
+        {
+            // move towards player
+            myRigidbody2D.velocity = (collider.transform.position - transform.position).normalized;
+        }
     }
 }
