3ff2b532c30a5fcf225ecb0a498c6858b0bbee7b
Sam Layton <slayton@uccs.edu>
2019-10-06 10:44:20 -0600

    Added static craftable items registry

---
+++ Terminus/Assets/Scripts/Managers/CraftableItemsRegistry.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains all combinations of crafting materials and
+/// the items (currently just weapons) they create.
+/// </summary>
+public static class CraftableItemsRegistry
+{
+    // private variables
+    static Dictionary<CraftingMaterials[], WeaponType> materialsToWeapons =
+        new Dictionary<CraftingMaterials[], WeaponType>();
+
+    /// <summary>
+    /// Initializes registry by pairing set of crafting materials with
+    /// craftable weapons.
+    /// TODO: Initialize by reading from file rather than hard-coding
+    /// combinations in this method.
+    /// </summary>
+    public static void Initialize()
+    {
+        // pair material combinations with craftable weapons
+        materialsToWeapons.Add( new CraftingMaterials[] { CraftingMaterials.biomass, CraftingMaterials.casing, CraftingMaterials.powder}, 
+            WeaponType.BioRifle);
+    }
+}
