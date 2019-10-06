af1ecfd056749ed3862c1ec47675efe64761fd2a
Sam Layton <slayton@uccs.edu>
2019-10-06 10:49:54 -0600

    Game Initializer initializes craftable items registry

---
+++ Terminus/Assets/Scripts/Managers/GameDataInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Initializes game's background data once once
+/// when application loads.
+/// </summary>
+public class GameDataInitializer : MonoBehaviour
+{
+    // private variables
+    bool initialized = false;
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // never initialize twice
+        if (!initialized)
+        {
+            initialized = true;
+
+            // initialize craftable items registry
+            CraftableItemsRegistry.Initialize();
+        }
+    }
+}
