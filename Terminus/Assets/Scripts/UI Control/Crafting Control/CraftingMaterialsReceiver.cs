b3e307aab46faeed2fef3ee1fa3b7c9204a03cdc
Sam Layton <slayton@uccs.edu>
2019-10-06 00:01:03 -0600

    Player can push materials onto crafting menu

--- Terminus/Assets/Scripts/UI Control/Crafting Control/CraftingMaterialsReceiver.cs
+++ Terminus/Assets/Scripts/UI Control/Crafting Control/CraftingMaterialsReceiver.cs
@@ -13,10 +13,11 @@ public class CraftingMaterialsReceiver : CraftingMaterialAdder
     // serialized variables
     [Range(1, 10)]
     [SerializeField] int maxMaterialsOnDeck = 3;                // max number of items player can push onto crafting menu
+    [SerializeField] Transform parentContainer;                 // parent transform of any material on deck in crafting menu
     [SerializeField] GameObject onDeckMaterialTemplate;         // generic UI representation of material added to crafting menu
     [SerializeField] Sprite[] craftingMaterialsIcons;           // list of sprites corresponding to each crafting material
                                                                 // NOTE: must be entered in order as they appear in CraftingMaterials enumeration
-    [SerializeField] Color[] iconColors;                        // list of sprite colors corresponding to each crafting material icon
+    [SerializeField] Color[] onDeckIconColors;                  // list of sprite colors corresponding to each crafting material icon
                                                                 // NOTE: like above, must be entered in same order as CraftingMaterials enumeration
 
     // private variables
@@ -39,7 +40,7 @@ public class CraftingMaterialsReceiver : CraftingMaterialAdder
     /// crafting menu's "On Deck" list
     /// </summary>
     /// <param name="materialToPush">type of material to push</param>
-    /// <param name="amount">IGNORED - needed to listen for particular event</param>
+    /// <param name="amount">IGNORED - needed to listen for remove materials event</param>
     void PushMaterial(CraftingMaterials materialToPush, int amount)
     {
         // if materials currently on deck isn't above max
@@ -47,7 +48,15 @@ public class CraftingMaterialsReceiver : CraftingMaterialAdder
         {
             // push them onto deck
             materialsOnDeck.Add(materialToPush);
+            CraftingMaterialOnDeck newOnDeck = Instantiate(onDeckMaterialTemplate, parentContainer).GetComponent<CraftingMaterialOnDeck>();
 
+            // modify visual elements of new on deck material
+            if (newOnDeck == null)
+                Debug.Log("null");
+
+            newOnDeck.Icon = craftingMaterialsIcons[(int)materialToPush];
+            newOnDeck.IconColor = onDeckIconColors[(int)materialToPush];
+            newOnDeck.MaterialType = materialToPush;
         }
         // otherwise (materials on deck plus new exceeds max)
         else
