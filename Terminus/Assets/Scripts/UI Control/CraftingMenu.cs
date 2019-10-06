ed0a4618463f90a92225e7aaa7fb274c5f79fcd7
Sam Layton <slayton@uccs.edu>
2019-10-05 22:51:31 -0600

    Added script to facilitate adding/removing items from crafting menu and making objects from said items

---
+++ Terminus/Assets/Scripts/UI Control/CraftingMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manages crafting menu, allowing players to push
+/// materials onto the menu, pop them off, and create
+/// items given the right materials.
+/// </summary>
+public class CraftingMenu : WeaponAdder
+{
+    // serialized variables
+    [SerializeField] GameObject onDeckMaterialTemplate;         // generic UI representation of material added to crafting menu
+    [SerializeField] Sprite[] craftingMaterialsIcons;           // list of sprites corresponding to each crafting material
+                                                                // NOTE: must be entered in order as they appear in CraftingMaterials enumeration
+    [SerializeField] Color[] iconColors;                        // list of sprite colors corresponding to each crafting material icon
+                                                                // NOTE: like above, must be entered in same order as CraftingMaterials enumeration
+
+    // private variables
+
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
