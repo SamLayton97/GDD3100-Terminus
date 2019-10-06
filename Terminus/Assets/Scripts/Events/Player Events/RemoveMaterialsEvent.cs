1b73e02176969cd73498b357d98871e51f196bd3
Sam Layton <slayton@uccs.edu>
2019-10-05 18:06:56 -0600

    Added event for deducting crafting materials from player's inventory

---
+++ Terminus/Assets/Scripts/Events/Player Events/RemoveMaterialsEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event invoked to deduct amount of given
+/// crafting material from players inventory.
+/// </summary>
+public class RemoveMaterialsEvent : UnityEvent<CraftingMaterials, int>
+{
+}
