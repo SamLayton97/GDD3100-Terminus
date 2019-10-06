635823fe0613db3170a12eb6b6ef4be8300f0bd9
Sam Layton <slayton@uccs.edu>
2019-10-06 01:24:03 -0600

    Added script to handle actually crafting the weapon

---
+++ Terminus/Assets/Scripts/UI Control/Crafting Control/WeaponCrafter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Determines whether player can craft a weapon
+/// from materials on deck, displaying weapon if
+/// that's the case, and adding weapon to player's
+/// inventory on user input.
+/// </summary>
+public class WeaponCrafter : WeaponAdder
+{
+
+}
