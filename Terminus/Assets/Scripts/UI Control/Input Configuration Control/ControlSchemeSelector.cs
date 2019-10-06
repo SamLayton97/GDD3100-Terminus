754fd5dd83b27fefc7a55473d1d8c68d0f25f1d6
Sam Layton <slayton@uccs.edu>
2019-10-06 16:29:47 -0600

    Buttons on option menu respond to user input

---
+++ Terminus/Assets/Scripts/UI Control/Input Configuration Control/ControlSchemeSelector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// An enumeration of keyboard control schemes
+/// </summary>
+//public enum ControlSchemes
+//{
+//    standard,
+//    specialist,
+//    leftHanded,
+//    leftySpecialist
+//}
+
+/// <summary>
+/// Manages swapping between control schemes from
+/// the options menu.
+/// </summary>
+public class ControlSchemeSelector : MonoBehaviour
+{
+    /// <summary>
+    /// Swaps player's control scheme to standard
+    /// </summary>
+    public void SwapToStandard()
+    {
+        Debug.Log("standard");
+    }
+
+    /// <summary>
+    /// Swaps player's control scheme to specialist
+    /// </summary>
+    public void SwapToSpecialist()
+    {
+        Debug.Log("specialist");
+    }
+
+    /// <summary>
+    /// Swaps player's control scene to left-handed
+    /// </summary>
+    public void SwapToLeftHanded()
+    {
+        Debug.Log("lefty");
+    }
+
+    /// <summary>
+    /// Swaps player's control scheme to lefty-specialist
+    /// </summary>
+    public void SwapToLeftySpecialist()
+    {
+        Debug.Log("l specialist");
+    }
+}
