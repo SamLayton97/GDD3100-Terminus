b15cb05bee7882112c5d6ee87a2ba00fb18cd5b8
Sam Layton <slayton@uccs.edu>
2019-10-18 19:39:55 -0600

    Player-wall collision particles destroy themselves after duration of effect

---
+++ Terminus/Assets/Scripts/Environment/DestroyParticleOnEnd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroys one-shot particle effects after they've ended
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class DestroyParticleOnEnd : MonoBehaviour
+{
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // destroy object after effect's duration
+        Destroy(gameObject, GetComponent<ParticleSystem>().main.duration);
+    }
+}
