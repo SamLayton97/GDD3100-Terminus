cc53751b283bf96537dac9697843bac825281829
Sam Layton <slayton@uccs.edu>
2019-10-19 16:01:11 -0600

    Added singleton support for closed captions

---
+++ Terminus/Assets/Scripts/UI Control/Miscellaneous/ClosedCaptions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Singleton controlling display of closed-captions
+/// throughout application.
+/// </summary>
+public class ClosedCaptions : MonoBehaviour
+{
+    // singleton support
+    static ClosedCaptions instance;
+
+    /// <summary>
+    /// Read-access property returning instance of
+    /// CC singleton.
+    /// </summary>
+    public static ClosedCaptions Instance
+    {
+        get { return instance; }
+    }
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // if singleton has already initialized as another instance
+        if (instance != null && Instance != this)
+        {
+            // destroy this instance and break
+            Destroy(gameObject);
+            return;
+        }
+
+        // set this object as instance of singleton
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
