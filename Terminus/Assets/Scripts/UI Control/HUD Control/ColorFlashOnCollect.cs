b99754b999bd2013b8b3f054728022c1bc39d79f
Sam Layton <slayton@uccs.edu>
2019-10-19 21:48:26 -0600

    Weapon HUD flashes when player collects a new weapon

--- Terminus/Assets/Scripts/UI Control/HUD Control/ColorFlashOnCollect.cs
+++ Terminus/Assets/Scripts/UI Control/HUD Control/ColorFlashOnCollect.cs
@@ -34,11 +34,47 @@ public class ColorFlashOnCollect : MonoBehaviour
     /// </summary>
     void Start()
     {
-        // TODO: add self as listener to add weapon event
-
+        // add self as listener to add weapon event
+        EventManager.AddPickUpWeaponListener(HandleWeaponPickup);
     }
 
-    
+    /// <summary>
+    /// Starts coroutine to flash HUD element when
+    /// player pickups up weapon.
+    /// </summary>
+    /// <param name="type">Weapon type collected.
+    /// NOTE: discarded; only needed to listen for Pick Up Weapon event.</param>
+    void HandleWeaponPickup(WeaponType type)
+    {
+        if (flashCoroutine != null) StopCoroutine(flashCoroutine);
+        flashCoroutine = FlashHUDElement(flashTime);
+        StartCoroutine(flashCoroutine);
+    }
 
+    /// <summary>
+    /// Shifts color of HUD element from one color to another and back
+    /// over given period of time.
+    /// </summary>
+    /// <param name="flashTime"></param>
+    /// <returns></returns>
+    IEnumerator FlashHUDElement(float flashTime)
+    {
+        // declare support variables
+        bool movingTo = true;
+        float lerpProgress = 0f;
 
+        // while image color hasn't returned to start
+        do
+        {
+            // shift color by increment, reversing direction at apex
+            lerpProgress += Time.deltaTime * (2f / flashTime) * (movingTo ? 1 : -1);
+            flashImage.color = Color.Lerp(fromColor, toColor, lerpProgress);
+            if (flashImage.color == toColor)
+                movingTo = !movingTo;
+
+            // wait a frame before lerping again
+            yield return new WaitForEndOfFrame();
+
+        } while (flashImage.color != fromColor);
+    }
 }
