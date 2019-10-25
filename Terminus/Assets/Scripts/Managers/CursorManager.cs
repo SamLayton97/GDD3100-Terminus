f332fb4cfc018d2cf3121b1d15a4132b6a245665
Sam Layton <slayton@uccs.edu>
2019-10-25 11:59:17 -0600

    Creating script to manage user's mouse cursor

---
+++ Terminus/Assets/Scripts/Managers/CursorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton managing design and size of user's mouse cursor.
+/// </summary>
+public class CursorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
