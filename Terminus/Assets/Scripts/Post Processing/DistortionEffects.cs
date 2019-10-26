da27be7c2cd6fe0222998877c608570381099d26
Sam Layton <slayton@uccs.edu>
2019-10-26 12:14:10 -0600

    Created script and object to control post-processing effects related to physical distortion

---
+++ Terminus/Assets/Scripts/Post Processing/DistortionEffects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+/// <summary>
+/// Reflects player's 'sanity'/physical distortion using 
+/// post-processing effects.
+/// </summary>
+public class DistortionEffects : PostProcessEffectController
+{
+
+}
