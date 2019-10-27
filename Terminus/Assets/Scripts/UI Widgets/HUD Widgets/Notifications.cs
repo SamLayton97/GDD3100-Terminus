6fcc24f14ecad115f8f78f7beb6cacb63d129766
Sam Layton <slayton@uccs.edu>
2019-10-27 10:27:37 -0600

    Notifications pull correct components

--- Terminus/Assets/Scripts/UI Widgets/HUD Widgets/Notifications.cs
+++ Terminus/Assets/Scripts/UI Widgets/HUD Widgets/Notifications.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 /// <summary>
 /// Displays notifications to the player on the HUD.
-/// Typically used for alerts like low oxygen.
+/// Typically used for alerts.
 /// </summary>
 [RequireComponent(typeof(CanvasGroup))]
 public class Notifications : MonoBehaviour
@@ -13,5 +13,17 @@ public class Notifications : MonoBehaviour
     // display support
     [SerializeField] Text notificationText;
     CanvasGroup myCanvasGroup;
+    RectTransform myTransform;
 
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // retrieve necessary components
+        myCanvasGroup = GetComponent<CanvasGroup>();
+        myTransform = GetComponent<RectTransform>();
+        if (notificationText == null)
+            notificationText = GetComponentInChildren<Text>();
+    }
 }
