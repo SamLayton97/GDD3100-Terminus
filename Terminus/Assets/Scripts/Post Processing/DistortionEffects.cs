36ee31c032914816ff7ca5ff07fe5248ed4f3bba
Sam Layton <slayton@uccs.edu>
2019-10-27 14:26:54 -0600

    Distortion effect controller calculates delta of sanity between frames

--- Terminus/Assets/Scripts/Post Processing/DistortionEffects.cs
+++ Terminus/Assets/Scripts/Post Processing/DistortionEffects.cs
@@ -13,6 +13,9 @@ public class DistortionEffects : PostProcessEffectController
     [Range(0f, 100f)]
     [SerializeField] float startHallucinating = 40f;        // point at which persistent hallucination effect begins playing
 
+    // support variables
+    float sanityLastFrame = 100f;
+
     /// <summary>
     /// Called before first frame of Update()
     /// </summary>
@@ -20,6 +23,7 @@ public class DistortionEffects : PostProcessEffectController
     {
         // add self as listener to appropriate events
         EventManager.AddUpdateSanityListener(ScaleHallucinationEffect);
+        EventManager.AddUpdateSanityListener(ScaleDistortionEffect);
     }
 
     /// <summary>
@@ -30,5 +34,18 @@ public class DistortionEffects : PostProcessEffectController
     void ScaleHallucinationEffect(float remainingSanity)
     {
         myVolumes[0].weight = Mathf.Max(0, (1 - (remainingSanity / startHallucinating)));
+    }
+
+    /// <summary>
+    /// Increases weight/volume of effects tied to 
+    /// negative change in player's sanity
+    /// </summary>
+    /// <param name="remainingSanity"></param>
+    void ScaleDistortionEffect(float remainingSanity)
+    {
+        Debug.Log("Delta: " + ((remainingSanity - sanityLastFrame) / Time.deltaTime));
+
+        // store sanity to calculate delta for next frame
+        sanityLastFrame = remainingSanity;
     }
 }
