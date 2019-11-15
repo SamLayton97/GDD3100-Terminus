915707c72d04919e37e4daee346f75312623ae85
Sam Layton <slayton@uccs.edu>
2019-11-15 16:33:57 -0700

    lazy fill coroutine starts whenever player loses oxygen

--- Terminus/Assets/Scripts/UI Widgets/HUD Widgets/O2Meter.cs
+++ Terminus/Assets/Scripts/UI Widgets/HUD Widgets/O2Meter.cs
@@ -7,10 +7,13 @@ using UnityEngine;
 /// </summary>
 public class O2Meter : MeterScaler
 {
-    // lazy fill configuration support
+    // lazy fill support variables
     [SerializeField] RectTransform lazyFill;
+    [Range(0f, 3f)]
     [SerializeField] float lazyFillWait = 1f;           // time 'lazy' oxygen meter fill waits before shrinking
+    [Range(0f, 10f)]
     [SerializeField] float lazyFillShrinkRate = 1f;     // rate at which meter shrinks
+    IEnumerator lazyFillCoroutine;                      // coroutine controlling oxygen meter's lazy fill
 
     /// <summary>
     /// Called before first frame of Update
@@ -28,7 +31,24 @@ public class O2Meter : MeterScaler
     /// <param name="newValue">new value from 0 - 100 to scale to</param>
     protected override void UpdateDisplay(float newValue)
     {
+        // start coroutine to shrink lazy fill
+        lazyFillCoroutine = ShrinkLazyFill();
+        StartCoroutine(lazyFillCoroutine);
+
         // scale meter
         base.UpdateDisplay(newValue);
+    }
+
+    /// <summary>
+    /// Shrinks oxygen meter's lazy fill to match 
+    /// scale of actual meter after waiting a period.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ShrinkLazyFill()
+    {
+        // lock lazy fill's scale for wait duration
+        Debug.Log("lock");
+        yield return new WaitForSeconds(lazyFillWait);
+        Debug.Log("unlock");
     }
 }
