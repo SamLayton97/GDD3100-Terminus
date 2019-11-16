b043dd86b3b2ee8db1b29c8664333faf0bbe7a58
Sam Layton <slayton@uccs.edu>
2019-11-16 14:46:12 -0700

    Created script that deactivates one-shot effects when they're done

---
+++ Terminus/Assets/Scripts/Environment/DeactivateParticleOnEnd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Deactivates one-shot particle effect after it has ended
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class DeactivateParticleOnEnd : MonoBehaviour
+{
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // start deactivation coroutine
+        LateDeactivate(GetComponent<ParticleSystem>().main.duration);
+    }
+
+    /// <summary>
+    /// Deactivates particle effect after duration
+    /// </summary>
+    /// <param name="duration">duration of effect</param>
+    /// <returns></returns>
+    IEnumerator LateDeactivate(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        gameObject.SetActive(false);
+    }
+}
