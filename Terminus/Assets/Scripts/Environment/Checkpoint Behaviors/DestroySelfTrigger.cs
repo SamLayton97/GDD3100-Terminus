23cf7b37ea7c191b932a36f23ffd06ebfb234b18
Sam Layton <slayton@uccs.edu>
2019-11-16 23:05:25 -0700

    Checkpoint can self destruct when player enters collision

---
+++ Terminus/Assets/Scripts/Environment/Checkpoint Behaviors/DestroySelfTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroys checkpoint when player collides with it.
+/// </summary>
+public class DestroySelfTrigger : MonoBehaviour
+{
+    /// <summary>
+    /// Called when player enters trigger box
+    /// </summary>
+    /// <param name="other">collision info</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // if player's body entered trigger
+        if (!other.isTrigger)
+        {
+            // self-destruct
+            Destroy(gameObject);
+        }
+    }
+}
