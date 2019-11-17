32d41f3a11e9b8a844d17a83764d5d8fa6171920
Sam Layton <slayton@uccs.edu>
2019-11-16 23:19:39 -0700

    Checkpoint can send notification to player

---
+++ Terminus/Assets/Scripts/Environment/Checkpoint Behaviors/NotifyTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sends brief notification to player when
+/// they collide with this checkpoint
+/// </summary>
+public class NotifyTrigger : MonoBehaviour
+{
+    // notification config variables
+    [SerializeField]
+    string message = "";            // notification displayed to player on trigger enter
+
+    /// <summary>
+    /// Called when player enters trigger box
+    /// </summary>
+    /// <param name="other">collision info</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // if player's body entered trigger
+        if (!other.isTrigger)
+        {
+            // send notification
+            Notifications.Instance.Display(message);
+
+        }
+    }
+}
