3924338b8b3c808719fb9e123296ea5265457404
Sam Layton <slayton@uccs.edu>
2019-11-16 22:31:20 -0700

    Created base class for all obejcts which deduct sanity

---
+++ Terminus/Assets/Scripts/Environment/Checkpoint Behaviors/SanityDeductor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Base class of all objects which can take away from player's sanity pool
+/// </summary>
+public abstract class SanityDeductor : MonoBehaviour
+{
+    // event support
+
+}
