763dc20d983b3488d4f529166e1ef67524fb2f44
Sam Layton <slayton@uccs.edu>
2019-12-02 15:21:48 -0700

    Created script to control rotation

---
+++ Terminus/Assets/Scripts/Player/Control/MobileFaceJoystickPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Rotates object to face direction of mobile joystick
+/// </summary>
+public class MobileFaceJoystickPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
