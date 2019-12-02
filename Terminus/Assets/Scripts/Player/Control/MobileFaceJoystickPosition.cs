479a36aa653da2701060f6d13ef4157f3c39c761
Sam Layton <slayton@uccs.edu>
2019-12-02 16:01:15 -0700

    player only rotates if joystick has input to give

--- Terminus/Assets/Scripts/Player/Control/MobileFaceJoystickPosition.cs
+++ Terminus/Assets/Scripts/Player/Control/MobileFaceJoystickPosition.cs
@@ -12,21 +12,26 @@ public class MobileFaceJoystickPosition : MonoBehaviour
     // configuration variables
     [SerializeField] float rotationSpeed = 10f;     // rate at which object turns to face joystick direction
 
+    // rotation support variables
+    float faceHorizontal = 0f;
+    float faceVertical = 0f;
+
     /// <summary>
     /// Called once per frame
     /// </summary>
     void Update()
     {
-        // if game isn't paused
-        if (Time.timeScale != 0)
-        {
-            // find angle of mobile joystick
-            float joystickAngle = Mathf.Atan2(CrossPlatformInputManager.GetAxis("AimVertical"),
-                CrossPlatformInputManager.GetAxis("AimHorizontal")) * Mathf.Rad2Deg;
+        // get joystick input
+        faceHorizontal = CrossPlatformInputManager.GetAxis("AimHorizontal");
+        faceVertical = CrossPlatformInputManager.GetAxis("AimVertical");
 
+        // if game isn't paused and player gave some input
+        if (Time.timeScale != 0 && !(faceHorizontal == 0 && faceVertical == 0))
+        {
             // turn object to face joystick direction
             Quaternion targetOrientation = new Quaternion();
-            targetOrientation.eulerAngles = new Vector3(0, 0, joystickAngle);
+            targetOrientation.eulerAngles = new Vector3(0, 0, 
+                Mathf.Atan2(faceVertical, faceHorizontal) * Mathf.Rad2Deg);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetOrientation, rotationSpeed);
         }
     }
