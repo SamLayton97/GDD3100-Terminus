fc01c05e514e77f7780f4d18c7604718b5f39931
Sam Layton <slayton@uccs.edu>
2019-12-02 18:12:40 -0700

    Created joystick background

---
+++ Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/JoystickBackgroundFadeIn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls fade-in and fade-out of mobile joystick's background
+/// when users holds the holds it down/releases hold
+/// </summary>
+[RequireComponent(typeof(CanvasGroup))]
+public class JoystickBackgroundFadeIn : MonoBehaviour
+{
+    // fade in configuration variables
+    [Range(0f, 5f)]
+    [SerializeField] float fadeInRate = 0f;     // rate at which background fades in and out
+
+    // support variables
+    CanvasGroup myCanvasGroup;
+
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // retrieve relevant components
+        myCanvasGroup = GetComponent<CanvasGroup>();
+    }
+    
+    /// <summary>
+    /// Initiates background fade in when user's
+    /// pointer depresses over joystick
+    /// </summary>
+    public void HandlePointerDown()
+    {
+        Debug.Log("here");
+    }
+
+    /// <summary>
+    /// Initiates background fade out when user's
+    /// pointer releases joystick
+    /// </summary>
+    public void HandlePointerUp()
+    {
+        Debug.Log("there");
+    }
+}
