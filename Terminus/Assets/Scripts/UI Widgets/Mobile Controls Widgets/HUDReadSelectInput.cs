58114077bea65e6ce14c57dfa70fe419884ceccf
Sam Layton <slayton@uccs.edu>
2019-12-02 20:05:22 -0700

    Created script to read pointer down input from weapons HUD

---
+++ Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/HUDReadSelectInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Reads pointer-based input from Weapons HUD, initiating
+/// selection of appropriate weapon in player's possession.
+/// Used for mobile controls.
+/// </summary>
+public class HUDReadSelectInput : MonoBehaviour
+{
+    // event support
+    HUDSelectWeaponEvent selectEvent;
+
+    /// <summary>
+    /// Used for late initialization
+    /// </summary>
+    void Start()
+    {
+        // TODO: add self as invoker of HUD select event
+        selectEvent = new HUDSelectWeaponEvent();
+
+    }
+}
