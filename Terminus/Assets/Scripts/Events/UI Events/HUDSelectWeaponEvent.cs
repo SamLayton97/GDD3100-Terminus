e5fcce3573e1a4bb525cddc2bed0cc180fac5747
Sam Layton <slayton@uccs.edu>
2019-12-02 20:01:39 -0700

    Added event to select weapon from HUD

---
+++ Terminus/Assets/Scripts/Events/UI Events/HUDSelectWeaponEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event used to select weapon in player's possession from
+/// weapon display HUD. Used for mobile controls.
+/// </summary>
+public class HUDSelectWeaponEvent : UnityEvent<int>
+{
+}
