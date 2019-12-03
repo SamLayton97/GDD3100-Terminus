9f81ab9586a9746343273497904c5e24cebaff90
Sam Layton <slayton@uccs.edu>
2019-12-02 22:36:11 -0700

    Button press initiates pop coroutine -- currently doesn't do anything

--- Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/PopButtonOnPress.cs
+++ Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/PopButtonOnPress.cs
@@ -18,13 +18,39 @@ public class PopButtonOnPress : MonoBehaviour
     // pop support variables
     CanvasGroup popCanvasGroup;                         // controls visibility of meter pop interaction
     Vector2 popPeakScale = new Vector2();               // scale meter pop grows to
-    IEnumerator popCoroutine;                           // coroutine controlling visibility and scale of pop image
 
     /// <summary>
     /// Used for initialization
     /// </summary>
     void Awake()
     {
+        // if not set in editor, assume component controls image on same gameobject
+        if (!popTransform)
+            popTransform = GetComponent<RectTransform>();
 
+        // retrieve relevant components/information
+        popCanvasGroup = popTransform.GetComponent<CanvasGroup>();
+        popPeakScale = popTransform.localScale;
     }
+
+    /// <summary>
+    /// Initiates 'pop' microinteraction when user presses button down
+    /// </summary>
+    public void HandlePress()
+    {
+        // initiate button pop
+        StartCoroutine(PopButton());
+    }
+
+    /// <summary>
+    /// Causes button to "pop" when user presses it
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator PopButton()
+    {
+        Debug.Log("here");
+
+        yield return new WaitForEndOfFrame();
+    }
+
 }
