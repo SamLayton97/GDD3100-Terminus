d47ae9885dc4df901cd837419d4ce0d22e41013e
Sam Layton <slayton@uccs.edu>
2019-12-03 21:30:48 -0700

    Created swipe zone on mobile controls prefab. Created script to handle detecting scripts

---
+++ Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/SwipeDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects user swipe gestures over pre-defined zone
+/// Based on source: https://www.youtube.com/watch?v=jbFYYbu5bdc
+/// </summary>
+public class SwipeDetector : MonoBehaviour
+{
+    // swipe configuration variables
+    [SerializeField]
+    bool detectSwipeOnlyAfterRelease = true;    // determines whether to detect swipe during or after gesture release
+
+    // swipe support variables
+    Vector2 fingerDownPosition;                 // finger screen position when user initiated swipe gesture
+    Vector2 fingerUpPosition;                   // finger screen position when user released swipe gesture
+
+
+}
+
+/// <summary>
+/// Enumeration of all possible swipe directions
+/// read by detector
+/// </summary>
+public enum SwipeDirections
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
