6bec6cd884d724827cdc93236062221141fe09a4
Sam Layton <slayton@uccs.edu>
2019-12-03 19:56:25 -0700

    Created script to register quick swap input from HUD

---
+++ Terminus/Assets/Scripts/UI Widgets/Mobile Controls Widgets/HUDReadSwapInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Reads pointer-based input from quick select HUD, 
+/// initiating weapon swapping in correct direction.
+/// Used for mobile controls.
+/// </summary>
+public class HUDReadSwapInput : MonoBehaviour
+{
+
+}
