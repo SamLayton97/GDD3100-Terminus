15eed33c8ac1f3b2c96a4d6e74a90430255b6493
Sam Layton <slayton@uccs.edu>
2019-12-03 19:49:51 -0700

    Created event to handle swapping weapons from HUD inputs

---
+++ Terminus/Assets/Scripts/Events/UI Events/HUDSwapWeaponEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event used to swao weapon in player's possession from
+/// quick swap HUD element. Used for mobile controls.
+/// </summary>
+public class HUDSwapWeaponEvent : UnityEvent<bool>
+{
+}
