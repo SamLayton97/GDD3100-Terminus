133df367cc12ac25a43776852b918b2e9c335832
Sam Layton <slayton@uccs.edu>
2019-12-09 22:44:01 -0700

    Created event for applying fire feedback

---
+++ Terminus/Assets/Scripts/Events/Player Events/ActivateFireFeedbackEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event invoked to activate physical and audio-visual
+/// feedback on player when they fire a weapon.
+/// </summary>
+public class ActivateFireFeedbackEvent : UnityEvent<float, WeaponType>
+{
+}
